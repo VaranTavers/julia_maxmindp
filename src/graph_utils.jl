33c7f9436f1b5c45880fc5b5e0bd29453e75a072
Tasnádi Zoltán <tasnadi98@gmail.com>
2023-01-22 15:05:30 +0200

    Created Weighted Edge List format

--- src/graph_utils.jl
+++ src/graph_utils.jl
@@ -2,13 +2,29 @@ begin
     using Graphs
     using SimpleWeightedGraphs
     using GraphIO
+		using CSV
+		using DataFrames
 end
+
+struct WELFormat <: Graphs.AbstractGraphFormat
+	delim
+	WELFormat() = new(",")
+	WELFormat(delim) = new(delim)
+end
+
+function Graphs.loadgraph(io::IO, gname::String, f::WELFormat)
+	g, _ = read_edge_list_weighted(io; delim=f.delim)
+
+	g
+end
+
+Graphs.loadgraphs(io::IO, f::WELFormat) = loadgraph(io, "...", f)
 
 function read_edge_list_weighted(filename; delim=" ")
 	csv = CSV.read(filename, DataFrame; header=false, delim=delim)
 	labels = unique(sort(vcat(csv[:, 1], csv[:, 2])))
 	n = length(labels)
-	g = SimpleWeightedGraph(csv[:, 1], csv[:, 2], csv[:, 3])
+	g = SimpleWeightedGraph(csv[:, 1], csv[:, 2], csv[:, 3]; combine=max)
 
 	g, labels
 end
