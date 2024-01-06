7a00b85490116374937ab8333431f67bf969b8a8
Tasnádi Zoltán <tasnadi98@gmail.com>
2024-01-06 19:20:55 +0200

    Fixes utils

--- src/utils/graph_utils.jl
+++ src/utils/graph_utils.jl
@@ -2,57 +2,17 @@ begin
     using Graphs
     using SimpleWeightedGraphs
     using GraphIO
-		using CSV
-		using DataFrames
-end
-
-struct WELFormat <: Graphs.AbstractGraphFormat
-	delim
-	WELFormat() = new(",")
-	WELFormat(delim) = new(delim)
-end
-
-function Graphs.loadgraph(io::IO, gname::String, f::WELFormat)
-	g, _ = read_edge_list_weighted(io; delim=f.delim)
-
-	g
-end
-
-Graphs.loadgraphs(io::IO, f::WELFormat) = loadgraph(io, "...", f)
-
-function read_edge_list_weighted(filename; delim=" ")
-	csv = CSV.read(filename, DataFrame; header=false, delim=delim)
-	if minimum(csv[:, 1]) == 0 || minimum(csv[:, 2]) == 0
-		csv .+= 1
-	end
-	labels = unique(sort(vcat(csv[:, 1], csv[:, 2])))
-	g = SimpleWeightedGraph(csv[:, 1], csv[:, 2], csv[:, 3]; combine=max)
-
-	g, labels
+    using CSV
+    using DataFrames
 end
 
 function getindex(elem, v)
     findfirst(x -> x == elem, v)
 end
 
-function subgraph(g, labels)
-    g_res = SimpleWeightedGraph(length(labels))
-    fa = zeros(nv(g))
-    for point in labels
-        fa[point] = 1
-    end
-    for edge in edges(g)
-        if fa[src(edge)] == 1 && fa[dst(edge)] == 1
-            add_edge!(g_res, getindex(src(edge), labels), getindex(dst(edge), labels), weight(edge))
-        end
-    end
-
-    g_res
-end
-
 function calculate_mindist(vertices, min_distances)
-	
-	dist_sums = map(i -> map(j -> min_distances[i, j], vertices), vertices)
 
-	minimum(map(sum,dist_sums))
+    dist_sums = map(i -> map(j -> min_distances[i, j], vertices), vertices)
+
+    minimum(map(sum, dist_sums))
 end
--- src/utils/maxima_extractor.jl
+++ src/utils/maxima_extractor.jl
@@ -4,33 +4,33 @@ using DataFrames
 # Extracts maxima from results_compiled files
 
 function find_main_file(files)
-  return "results_compiled.csv"
+    return "results_compiled.csv"
 end
 
-folders = readdir("results/")
+folders = readdir("../results/")
 
 summary_df_max = []
 summary_df_mean = []
 df_init = false
 
 for folder in folders
-  if isdir("results/$(folder)")
-    files = readdir("results/$(folder)")
+    if isdir("../results/$(folder)")
+        files = readdir("../results/$(folder)")
 
-    main_file = find_main_file(files)
+        main_file = find_main_file(files)
 
-    if isfile("results/$(folder)/$(main_file)")
-      df = CSV.read("results/$(folder)/$(main_file)", DataFrame)
-      if !df_init
-        global summary_df_max = df[:, ["graphs"]]
-        global summary_df_mean = df[:, ["graphs"]]
-        global df_init = true
-      end
-      summary_df_max[:, "$(folder)"] = df[:, "max"]
-      summary_df_mean[:, "$(folder)"] = df[:, "mean"]
+        if isfile("../results/$(folder)/$(main_file)")
+            df = CSV.read("../results/$(folder)/$(main_file)", DataFrame)
+            if !df_init
+                global summary_df_max = df[:, ["graphs"]]
+                global summary_df_mean = df[:, ["graphs"]]
+                global df_init = true
+            end
+            summary_df_max[:, "$(folder)"] = df[:, "max"]
+            summary_df_mean[:, "$(folder)"] = df[:, "mean"]
+        end
     end
-  end
 end
 
-CSV.write("results/compile_max.csv", summary_df_max)
-CSV.write("results/compile_mean.csv", summary_df_mean)
+CSV.write("../results/compile_max.csv", summary_df_max)
+CSV.write("../results/compile_mean.csv", summary_df_mean)
